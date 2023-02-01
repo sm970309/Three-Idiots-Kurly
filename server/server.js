977281fe8ce8521c0acf8c5194287db951921c59
sm970309 <sm970309@gmail.com>
2023-02-01 12:07:11 +0900

    상품 리스트 등록 API

--- server/server.js
+++ server/server.js
@@ -4,9 +4,18 @@ const path = require('path')
 const bodyParser = require('body-parser')
 const PORT = 8000;
 const app = express();
-const upload = require('./components/upload')
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+const upload = require('./components/upload');
+
+const {fb} = require("./components/fb");
+const {collection,addDoc, getFirestore} = require('firebase/firestore');
+const db = getFirestore(fb);
+
+app.use(express.json());
+app.use(express.urlencoded({
+    extended:false
+}))
+// app.use(bodyParser.urlencoded({ extended: false }))
+// app.use(bodyParser.json())
 
 // 서버와 클라이언트 연동 -> 서버에 접속할때는 8000 PORT로 접근
 
@@ -24,11 +33,24 @@ app.get('/items/:num',(req,res) =>{
 )
 app.get('/items',(req,res) =>{
     res.send('상품페이지')
-    console.log(upload.uploadItem)
+
 })
-app.post('/test',(req,res)=>{
-    console.log(req.body)
-    res.send('yes')
+
+// 상품 리스트 등록 API
+app.post('/uploaditems',async( req,res)=>{
+    const {no,name,price,img} = req.body;
+    console.log(no,name,price,img)
+    try{
+        await addDoc(collection(db,'items'),{
+            'no':no,
+            'name':name,
+            'price':price,
+            'img':img
+        })
+        res.send('complete')
+    }catch(e){
+        console.error("Error adding document: ", e);
+    }
 })
 
 app.listen(PORT,() =>{
