4764c887938aeabcf4cc442703ea559108ec8366
brojr <leehy9351@gmail.com>
2023-02-09 18:49:35 +0900

    배너 구현

--- client/src/components/ItemList.js
+++ client/src/components/ItemList.js
@@ -2,11 +2,13 @@ import PaintItem from "./PaintItem";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import React, { useEffect, useState } from "react";
+import SwiperItems from "./SwiperItems";
 
 const ItemList = () => {
   let items = JSON.parse(localStorage.getItem("items"));
   items = items.slice(0, 8);
 
-  return <PaintItem items={items} />;
+  // return <PaintItem items={items} />;
+  return <SwiperItems items={items} />;
 };
 export default ItemList;
