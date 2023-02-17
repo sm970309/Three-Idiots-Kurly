d23cf4a2d8efe48d27ac5d36269a9f0aebe14773
wb980609 <jwb6739@gmail.com>
2023-02-17 16:41:47 +0900

    회원가입 폼(주소 API 띄우는 것까지 완료)

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,11 @@ const API_KEY = "http://localhost:8000/items";
 
 function App() {
   let [items, setItems] = useState(null);
+  let [address, setAddress] = useState(null);
+
   localStorage.setItem("items", JSON.stringify(items));
+  localStorage.setItem("address", JSON.stringify(address));
+  
   const getItems = async () => {
     try {
       const response = await axios.get(API_KEY);
