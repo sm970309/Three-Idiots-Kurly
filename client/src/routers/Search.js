ed749f08cb7579efeb166548f9e08faf6c2b0edc
wb980609 <jwb6739@gmail.com>
2023-03-06 14:02:50 +0900

    예외처리 1차

---
+++ client/src/routers/Search.js
@@ -0,0 +1,14 @@
+import Navigation from "../components/Navigation";
+
+const Search = () => {
+
+    return (
+      <div>
+        <div>
+          <Navigation />
+        </div>
+      </div>
+    );
+  };
+  export default Search;
+  
